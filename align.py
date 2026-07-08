95f7f69e377e0dd49f3f18ff11442b9892dcaec2
thePhanMan15 <andrewphan2007@gmail.com>
2026-07-07 23:09:49 -0400

    Wrote notes_to_midi_list function for align.py

--- align.py
+++ align.py
@@ -1,4 +1,28 @@
+import librosa
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
-distance, path = fastdtw(recording, reference, dist=euclidean)
+def notes_to_midi_list(notes):
+    """
+        Converts a list of Note objects into a list of MIDI note numbers.
+ 
+        Parameters:
+        - notes (list): List of Note objects.
+ 
+        Returns:
+        - midi_list (list): List of MIDI note numbers.
+    """
+    midi_list = []
+
+    for i, note in enumerate(notes):
+        midi_num = librosa.note_to_midi(note.pitch)
+        midi_list.append(midi_num)
+    
+    return midi_list
+
+def align(reference, performance):
+    """
+        Aligns 
+    """
+
+    _, path = fastdtw(reference, performance, dist=euclidean)
